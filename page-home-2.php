8b1e83e8ade53fe8e7c9fcfa555091821f7ede03
Yan <yavallejor@gmail.com>
2020-06-13 00:30:43 -0500

    Get information tax coleccion

--- page-home-2.php
+++ page-home-2.php
@@ -41,53 +41,52 @@ get_header(); ?>
         </div>
     </section>
     <section class="ourproducts">
+
+        
         <div class="container mx-auto collectionSlick">
+            <?php
+                $terms = get_terms([
+                    'taxonomy' => 'coleccion',
+                    'hide_empty' => false,
+                ]);
+                
+                if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+                    
+                    foreach ( $terms as $term ) {
+
+                        $termino = get_term( $term->term_id, 'coleccion' );
+                        $termino_link = get_term_link( $termino );
+                        $icon = get_field('imagen_coleccion', $term->taxonomy . '_' . $term->term_id);
+                        $terminoDescription = get_field('informacion_coleccion', $term->taxonomy . '_' . $term->term_id); 
+            ?>
+                        <article class="collection">
+                            <div class="collection__left">
+                                <img src="<?php echo $icon; ?>" alt="" srcset="">
+                            </div>
+                            <div class="collection__right">
+                                <h2 class="title__section title__section--gold collection__right--title">
+                                    <?php echo $term->name; ?>
+                                </h2>
+                                <div class="line line--gold"></div>
+                                <div class="collection__right--description">
+                                    <p><?php echo $terminoDescription; ?></p>
+                                </div>
+                                <div class="collection__right--btn">
+                                    <a href="<?php echo $termino_link; ?>" class="btn btn--primary">Ver +</a>
+                                </div>
+                            </div>
+                        </article><!-- Collection -->
+                         
+
+            <?php
+                    }
+                    
+                }
+
+        ?>
             
-            <article class="collection">
-                <div class="collection__left">
-                    <img src="http://marta.local/wp-content/uploads/2020/06/coleccion-lynx.png" alt="" srcset="">
-                </div>
-                <div class="collection__right">
-                    <h2 class="title__section title__section--gold collection__right--title">Colección Gold & Black</h2>
-                    <div class="line line--gold"></div>
-                    <div class="collection__right--description">
-                        <p>Lorem IpsuLorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    </div>
-                    <div class="collection__right--btn">
-                        <a href="#" class="btn btn--primary">Ver +</a>
-                    </div>
-                </div>
-            </article><!-- Collection -->
-            <article class="collection">
-                <div class="collection__left">
-                    <img src="http://marta.local/wp-content/uploads/2020/06/coleccion-lynx.png" alt="" srcset="">
-                </div>
-                <div class="collection__right">
-                    <h2 class="title__section title__section--gold collection__right--title">Colección Gold & Black</h2>
-                    <div class="line line--gold"></div>
-                    <div class="collection__right--description">
-                        <p>Lorem IpsuLorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    </div>
-                    <div class="collection__right--btn">
-                        <a href="#" class="btn btn--primary">Ver +</a>
-                    </div>
-                </div>
-            </article><!-- Collection -->
-            <article class="collection">
-                <div class="collection__left">
-                    <img src="http://marta.local/wp-content/uploads/2020/06/coleccion-lynx.png" alt="" srcset="">
-                </div>
-                <div class="collection__right">
-                    <h2 class="title__section title__section--gold collection__right--title">Colección Gold & Black</h2>
-                    <div class="line line--gold"></div>
-                    <div class="collection__right--description">
-                        <p>Lorem IpsuLorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    </div>
-                    <div class="collection__right--btn">
-                        <a href="#" class="btn btn--primary">Ver +</a>
-                    </div>
-                </div>
-            </article><!-- Collection -->
+            
+            
          </div><!-- Collection Section -->
         
         <article class="ourproducts__margin">
