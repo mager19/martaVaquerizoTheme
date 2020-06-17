a330719afcdca84a5dad7bc4ad6ef15043b52679
Mario Germán Reyes C <mager19@gmail.com>
2020-06-16 20:34:15 -0500

    progress page.

---
+++ inc/hero-content.php
@@ -0,0 +1,16 @@
+<?php
+/*hero content*/
+?>
+
+<section class="hero-content">
+    <div class="container mx-auto">
+        <div class="row">
+            <div class="w-3/4 mx-auto">
+                <div class="title__section title__section--gold">
+                    <?php the_title(); ?>
+                </div>
+                <div class="line line--gold"></div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ page.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * The template for displaying Home page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package gforce
+ */
+
+get_header(); ?>
+
+<?php get_template_part("inc/hero", "content"); ?>
+
+<?php
+
+while (have_posts()) : the_post(); ?>
+    <div class="container mx-auto content-area">
+        <div class="row">
+            <div class="w-full">
+                <div class="title__section title__section--gold">
+                    <?php the_title(); ?>
+                </div>
+            </div>
+            <div class="w-full">
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+<?php endwhile; ?>
+<!-- post navigation -->
+
+<?php
+get_footer();
