c040701bcd511796a61d24264f339f046a84e6e0
Yan <yavallejor@gmail.com>
2020-06-18 22:29:50 -0500

    Get data collection

--- taxonomy-coleccion.php
+++ taxonomy-coleccion.php
@@ -26,13 +26,14 @@ get_header(); ?>
     <section class="hero__collection">
        <div class="container mx-auto hero__collection--box">
             <figure class="hero__collection--image md:w-4/12 lg:w-5/12">
-                    <img src="http://marta.local/wp-content/uploads/2020/06/image-coleccion.jpg" alt="" />
+                <?php $imageColeccion = get_field('imagen_pequena', $taxonomy. '_' . $id); ?>
+                <img src="<?php echo $imageColeccion; ?>" alt="" />
             </figure>
             <div class="hero__collection--information md:w-6/12 lg:w-5/12 md:ml-auto">
-                <h1 class="title__section">Colección Gold & Black</h1>
+                <h1 class="title__section"><?php echo $name; ?></h1>
                 <div class="line line--gold mb-5"></div>
-                <p>Colección que nace de mi fascinación por la combinación cromática del negro y dorado, colores corporativos de la marca.</p>
-                <p>Es una colección dinámica, en contínuo crecimiento, que albergará diseños que mostarán esas influencias tan diversas con las que me identifico.</p>
+                <?php $resumenColeccion = get_field('resumen_coleccion', $taxonomy. '_' . $id); ?>
+                <?php echo $resumenColeccion; ?>
             </div>
        </div>
     </section><!-- Hero Image -->
@@ -55,9 +56,7 @@ get_header(); ?>
                         'taxonomy' => 'coleccion',
                         'term' => $slug,
                     );
-                    
                     $query = new WP_Query( $args );
-                    
                     ?>
                     
                     <?php if( $query->have_posts() ) : ?>
@@ -65,54 +64,43 @@ get_header(); ?>
                         <?php while ( $query->have_posts() ) : $query->the_post(); 
                               global $product;
                               $product = get_product( get_the_ID() ); ?>
-                            <div class="ourproducts__item">
-                                <div class="ourproducts__item--image">
-                                    <figure>
-                                        <a href="<?php the_permalink( ); ?>">
-                                            <?php 
-                                                the_post_thumbnail( 'full' );
-                                            ?>
-                                            
-                                        </a>
-                                    </figure>
-                                    <div class="ourproducts__item--imageShare">
-                                        <ul>
-                                            <li><a href="<?php the_permalink(); ?>"><i class="marta-search"></i></a></li>
-                                            <li><?php echo do_shortcode( '[yith_wcwl_add_to_wishlist]' ); ?></li>
-                                            <li><a href="<?php the_permalink(); ?>"><i class="marta-loop2"></i></a></li>
-                                            <li><a href="<?php echo $product->add_to_cart_url(); ?>"><i class="marta-cart"></i></a></li>
-                                            
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="ourproducts__item--information">
-                                    <div class="ourproducts__item--informationLeft">
-                                        <h3 class="title--product">
-                                            <a href="<?php the_permalink( ); ?>"><?php the_title(); ?></a>
-                                        </h3>
-                                        <div class="ourproducts__item--informationPrice">
-                                            <span>Precio:</span>
-                                            <?php $price = get_post_meta( get_the_ID(), '_price', true ); ?>
-                                            <?php echo wc_price( $price ); ?>
-                                            
+                                <div class="ourproducts__item">
+                                    <div class="ourproducts__item--image">
+                                        <figure>
+                                            <a href="<?php the_permalink( ); ?>">
+                                                <?php the_post_thumbnail( 'full' ); ?>
+                                            </a>
+                                        </figure>
+                                        <div class="ourproducts__item--imageShare">
+                                            <ul>
+                                                <li><a href="<?php the_permalink(); ?>"><i class="marta-search"></i></a></li>
+                                                <li><?php echo do_shortcode( '[yith_wcwl_add_to_wishlist]' ); ?></li>
+                                                <li><a href="<?php the_permalink(); ?>"><i class="marta-loop2"></i></a></li>
+                                                <li><a href="<?php echo $product->add_to_cart_url(); ?>"><i class="marta-cart"></i></a></li>
+                                            </ul>
                                         </div>
                                     </div>
-                                    <div class="ourproducts__item--informationRight">
-                                        <?php echo wc_get_product_category_list($product->get_id()) ?> 
-                                        
+                                    <div class="ourproducts__item--information">
+                                        <div class="ourproducts__item--informationLeft">
+                                            <h3 class="title--product">
+                                                <a href="<?php the_permalink( ); ?>"><?php the_title(); ?></a>
+                                            </h3>
+                                            <div class="ourproducts__item--informationPrice">
+                                                <span>Precio:</span>
+                                                <?php $price = get_post_meta( get_the_ID(), '_price', true ); ?>
+                                                <?php echo wc_price( $price ); ?>
+                                            </div>
+                                        </div>
+                                        <div class="ourproducts__item--informationRight">
+                                            <?php echo wc_get_product_category_list($product->get_id()) ?> 
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
                         <?php endwhile; ?>
                         
                     <?php endif; ?>
                     
                     <?php wp_reset_query(); ?>
-                    
-
-
-                    
-                    
                 </div>
             </div>
         </article><!-- Nuestros Productos -->
