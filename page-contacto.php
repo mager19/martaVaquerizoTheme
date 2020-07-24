1757f39985a2435d89563566a92f5b0951e86c6c
Yan <yavallejor@gmail.com>
2020-07-23 22:26:17 -0500

    fix page contact

--- page-contacto.php
+++ page-contacto.php
@@ -22,48 +22,12 @@ get_header(); ?>
 while (have_posts()) : the_post(); ?>
     <div class="container mx-auto content-area contacto">
         <div class="flex flex-wrap">
-            
+
             <div class="w-full px-4">
                 <?php the_content(); ?>
             </div>
 
-            <div class="w-full lg:w-2/3 px-4 mt-12">
-                <?php the_field("form"); ?>
-            </div>
 
-            <div class="w-2/3 mx-auto lg:w-1/3 px-4 mt-12">
-                <div class="items__emails lg:pl-8 lg:pt-6">
-                    <?php if (have_rows('emails', 'option')) : ?>
-                        <?php while (have_rows('emails', 'option')) : the_row(); ?>
-                            <a href="mailto:<?php the_sub_field('email', 'option'); ?>" class="mb-4">
-                                <?php the_sub_field('email', 'option'); ?>
-                            </a>
-                        <?php endwhile; ?>
-                    <?php endif; ?>
-                </div>
-                <div class="contacto__links lg:pl-8">
-                    <div class="socialnetworks">
-                        <div class="socialnetworks--hashtag">
-                            <strong><?php the_field("hashtag", 'option'); ?></strong>
-                        </div>
-                        <ul class="socialnetworks__item mt-4">
-                            <?php if (have_rows('social_icons', 'option')) : ?>
-
-                                <?php while (have_rows('social_icons', 'option')) : the_row(); ?>
-                                    <li>
-                                        <a href="<?php the_sub_field('social_profile', 'option'); ?>">
-                                            <i class="marta-<?php the_sub_field('social_icon', 'option'); ?>"></i>
-                                        </a>
-                                    </li>
-
-                                <?php endwhile; ?>
-
-                            <?php endif; ?>
-
-                        </ul>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 <?php endwhile; ?>
